89ce1c00a4facd847d955b2bb7fc255c3fde4ec0
FoxGeoff <fox.geoffrey@gmail.com>
2019-03-15 18:39:24 -0700

    Ad code to upload component

--- client/src/app/upload/upload.component.ts
+++ client/src/app/upload/upload.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { DialogComponent } from './dialog/dialog.component';
+import { UploadService } from './upload.service';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent {
+  constructor(public dialog: MatDialog, public uploadService: UploadService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  public openUploadDialog() {
+    let dialogRef = this.dialog.open(DialogComponent, { width: '50%', height: '50%' });
   }
-
 }
